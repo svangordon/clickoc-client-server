03f4a72306c682df451ebb3f6aaba93de6bb7a02
fangs073 <stephen.van.gordon@gmail.com>
2016-07-26 16:46:45 -0600

    created get legislator actions

---
+++ src/redux/modules/legislator.js
@@ -0,0 +1,44 @@
+const LOAD = 'clickoc-client/legislator/LOAD';
+const LOAD_SUCCESS = 'clickoc-client/legislator/LOAD_SUCCESS';
+const LOAD_FAIL = 'clickoc-client/legislator/LOAD_FAIL';
+
+const initialState = {
+  loaded: false
+};
+
+export default function info(state = initialState, action = {}) {
+  switch (action.type) {
+    case LOAD:
+      return {
+        ...state,
+        loading: true
+      };
+    case LOAD_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        legislators: action.result
+      };
+    case LOAD_FAIL:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error: action.error
+      };
+    default:
+      return state;
+  }
+}
+
+export function isLoaded(globalState) {
+  return globalState.info && globalState.info.loaded;
+}
+
+export function load() {
+  return {
+    types: [LOAD, LOAD_SUCCESS, LOAD_FAIL],
+    promise: (client) => client.get('/legislator')
+  };
+}
