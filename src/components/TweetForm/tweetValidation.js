eea7699280ffbe3050681e3a9d81a3960c9ea5d3
svangordon <stephen.van.gordon@gmail.com>
2016-10-05 23:50:09 -0600

    shorten tweet length to allow for hashtag

--- src/components/TweetForm/tweetValidation.js
+++ src/components/TweetForm/tweetValidation.js
@@ -2,6 +2,6 @@ import memoize from 'lru-memoize';
 import {createValidator, required, maxLength} from 'utils/validation';
 
 const tweetValidation = createValidator({
-  tweetContent: [required, maxLength(125)] // 140 - 15 (max length of handle)
+  tweetContent: [required, maxLength(113)] // 140 - 15 (max length of handle) - 12 (length of clickoc hashtag)
 });
 export default memoize(10)(tweetValidation);
